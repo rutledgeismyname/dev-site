44fe9330cc37c9611745ae920787297228479c3f
rutledgeismyname <chrisrutledge@Chriss-MacBook-Pro.local>
2021-03-11 09:14:59 -0500

    fix loader

--- js/script.js
+++ js/script.js
@@ -175,6 +175,7 @@ batteryPromise.then(batteryCallback);
 function batteryCallback(batteryObject) {
   printBatteryStatus(batteryObject);
 }
+
 function printBatteryStatus(batteryObject) {
   if (batteryObject.level >= 0.9) {
     document.querySelector(
